87c3623b47d294cc7ac71afeea5f9db815e2768c
sooko <hansmotor89@gmail.com>
2019-10-04 19:50:14 +0700

    First commit Message

--- theme/__init__.py
+++ theme/__init__.py
@@ -7,19 +7,75 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty,NumericProperty,ListProperty,DictProperty
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.garden.navigationdrawer import NavigationDrawer
+from kivy.uix.behaviors.touchripple import  TouchRippleButtonBehavior
+from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.app import App
 Builder.load_string('''
+<RootMenu>:
+    orientation:"vertical"
+    size_hint:1,None
+    height:self.minimum_height
+<MenuButton>:
+    size_hint:1,None
+    height:"50sp"
+    canvas:
+        Color:
+            rgba:1,1,1,.2
+        Rectangle:
+            size:self.width,1
+            pos:self.pos
+<Menu>:
+    orientation:"vertical"
+    FloatLayout:
+        canvas:
+            Color:
+                rgba:1,1,1,1
+            Rectangle:
+                size:self.width,1
+                pos:self.pos
+        size_hint:1,.3
+        FloatLayout:
+            size_hint:None,None
+            pos_hint:{"center_x":.5,"center_y":.6}
+            size: 0.6*min(root.size), 0.6*min(root.size)
+            canvas:
+                Color:
+                    rgba:1,1,1,1
+                Ellipse:
+                    size:self.size
+                    pos:self.pos
+                    source:root.icon
+        BoxLayout:
+            size_hint:1,.2
+            pos_hint:{"center_x":.5,"y":0}
+            orientation:"vertical"
+            Label:
+                text:root.user_name
+                halign:"center"
+                valign:"bottom"
+                text_size:self.size
+                font_size:self.height
+            Label:
+                text:root.server
+                font_size:self.height
+                
+    ScrollView:        
+        RootMenu:
+            id:rm
+            on_parent:
+                root.add()
 <BtnMenu>:
 <stdTheme>:
     anim_type:"slide_above_simple"
-    Button
+    Menu:
     BoxLayout:
         orientation:"vertical"
         NavBar:
             on_menu_release:root.on_menu_release()
             on_option_release:root.on_option_release()
         Label
+        
 <Navbar>:
     size_hint:.98,None
     height:"50sp"
@@ -59,6 +115,10 @@ Builder.load_string('''
             root.option_button_color[3]=1
             root.option_release+=1
 ''')
+class RootMenu(BoxLayout):
+    pass
+class MenuButton(TouchRippleButtonBehavior,Label):
+    pass
 class BtnMenu(ButtonBehavior,Image):
     pass
 class BtnOption(ButtonBehavior,Image):
@@ -76,15 +136,28 @@ class NavBar(BoxLayout):
     option_button_color=ListProperty([1,1,1,1])
     def __init__(self,*args,**kwargs):
         super(NavBar,self).__init__(**kwargs)
-class StdTheme(NavigationDrawer):
+class Menu(BoxLayout):
+    user_name=StringProperty("")
+    server=StringProperty("")
+    list_text=ListProperty(["home","acount","setting"])
+    icon=StringProperty("asset/python.png")
     def __init__(self,*args,**kwargs):
-        super(StdTheme,self).__init__(**kwargs)
-    def on_menu_release(self):
-        self.toggle_state()
-    def on_option_release(self):
-        print("option_release")
-class Theme(App):
-    def build(self):
-        return StdTheme()
-if __name__=="__main__":
-    Theme().run()
+        super(Menu,self).__init__(**kwargs)
+    def add(self):
+        for i in self.list_text:
+            self.ids["rm"].add_widget(MenuButton(text=i,on_press=self.on_menu_choose))
+    def on_menu_choose(self,instance):
+        print(instance.text)
+# class StdTheme(NavigationDrawer):
+#     def __init__(self,*args,**kwargs):
+#         super(StdTheme,self).__init__(**kwargs)
+#     def on_menu_release(self):
+#         self.toggle_state()
+#     def on_option_release(self):
+#         print("option_release")
+#
+# class Theme(App):
+#     def build(self):
+#         return StdTheme()
+# if __name__=="__main__":
+#     Theme().run()
--- theme/test.py
+++ theme/test.py
@@ -1,8 +1,10 @@
 from __init__ import NavBar
 from kivy.app import App
 
+
 class MyApp(App):
     def build(self):
         return NavBar()
+
 if __name__=="__main__":
     MyApp().run()
