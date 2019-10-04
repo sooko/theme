3f56be1f9760842ee99a74a73a62e0f09098d93e
sooko_io <42238551+sooko@users.noreply.github.com>
2019-10-04 15:18:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
 
+setup(name='theme',
+      version='0.1',
+      description='theme',
+      url='https://github.com/sooko/twis.git',
+      author='Burhan',
+      author_email='hansmotor89@gmail.com',
+      license='MIT',
+      packages=['theme'],
+#       install_requires=['twisted'],
+      zip_safe=False)
