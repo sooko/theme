5ed64a38c9478510fdecd89f56057ffa8e522240
sooko <hansmotor89@gmail.com>
2019-10-08 03:14:40 +0700

    First commit

--- theme/__init__.py
+++ theme/__init__.py
@@ -14,8 +14,51 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.event import EventDispatcher
 from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.uix.behaviors import ButtonBehavior, ToggleButtonBehavior
+from kivy.uix.image import Image
+from kivy.properties import StringProperty, NumericProperty, ListProperty
+from kivy.uix.floatlayout import FloatLayout
 
 Builder.load_string('''
+<NavbarScan>:
+    size_hint:1,None
+    height:"30sp"
+    BtnImg:
+        color:root.btn_menu_color
+        source:"asset/3btn.png"    
+        size_hint:None,1
+        width:"30sp"
+        on_press:
+            self.color=0,1,1,.5
+            root.on_press_menu()
+        on_release:
+            self.color=root.btn_menu_color
+            root.on_release_menu()
+    Label
+        text:root.app_name
+        font_size:self.height/1.5
+        halign:"right"
+        valign:"middle"
+        text_size:self.size
+        color:root.app_name_color
+    Label:
+        size_hint:None,1
+        width:"10sp"
+    BtnImg:
+        id:qrbtn
+        color:root.btn_menu_color
+        source:"asset/qr.png"    
+        size_hint:None,1
+        width:"20sp"
+        on_press:
+            self.color=0,1,1,.5
+            root.scanbtn+=1
+        on_release:
+            self.color=root.btn_menu_color
+
 <AcountScreen>:
     pos_hint:{"center_x": .5, "center_y": self.pos_anim}
     Button:
@@ -452,3 +495,42 @@ class Menu(BoxLayout):
 #         return StdTheme()
 # if __name__=="__main__":
 #     Theme().run()
+
+
+
+Builder.load_string('''
+
+
+
+''')
+
+#
+# class BtnImg(ButtonBehavior, Image):
+#     pass
+
+
+class NavBarScan(BoxLayout):
+    scanbtn = NumericProperty(0)
+    menu_pressed = StringProperty("normal")
+    menu_released = StringProperty("down")
+    app_name = StringProperty("My Aplication")
+    btn_menu_color = ListProperty([1, 1, 1, 1])
+    app_name_color = ListProperty([1, 1, 1, 1])
+
+    def __init__(self, *args, **kwargs):
+        super(NavBar, self).__init__(*args, **kwargs)
+
+    def on_press_menu(self):
+        self.menu_pressed = "down"
+        self.menu_released = "normal"
+
+    def on_release_menu(self):
+        self.menu_released = "down"
+        self.menu_pressed = "normal"
+
+    def press(self, a, b):
+        print(a, b)
+
+    def on_scanbtn(self, a, b):
+        pass
+
--- theme/test.py
+++ theme/test.py
@@ -1,10 +1,14 @@
 from __init__ import NavBar
 from kivy.app import App
 
-
-class MyApp(App):
-    def build(self):
-        return NavBar()
-
-if __name__=="__main__":
-    MyApp().run()
+# a=[[1,2,3,4,5],[6,7,8,9,10]]
+# for i in a:
+#     print(i[0])
+#
+#
+# # class MyApp(App):
+# #     def build(self):
+# #         return NavBar()
+# #
+# # if __name__=="__main__":
+# #     MyApp().run()
